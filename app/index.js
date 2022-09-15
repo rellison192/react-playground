802d1d29df734642eb68fce25ad41c86132e0b04
Ryan Ellison <rellison@wayfair.com>
2022-09-14 21:59:05 -0400

    adding startup files for app

---
+++ app/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from "react-dom/client"
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>Hello Meh My old Freind</p>
+      </div>
+    )
+  }
+}
+
+
+const root = ReactDOM.creatRoot(document.getElementById("app");
+root.render(<App/>)
